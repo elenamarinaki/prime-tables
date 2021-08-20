7fc3ce34a43f97c045603e329a324d87be2c40e5
Elena <rhapsodyenmauve@gmail.com>
2021-08-20 01:09:04 +0100

    makePrimeTable function this function multiplies the primes array produced by the  with itself, producing a table.

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ console.log([p1, p2, p3]);
 // -----------
 
 function giveNPrimeNumbers(N) {
-  let primesArr = [null];
+  let primesArr = [' '];
   let isPrime;
 
   // we use the generator function to yield N number of primes
@@ -54,3 +54,27 @@ giveNPrimeNumbers(5);
 giveNPrimeNumbers(17);
 // expected: [ null, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59 ]
 // -----------
+
+function makePrimeTable(N) {
+  const primesArray = giveNPrimeNumbers(N);
+  // using a string variable which we are going to reuse every time we produce a new row of the table
+  let rowOutput = '';
+
+  // output the first row of primes - the original primes array
+  primesArray.forEach((p) => (rowOutput += `${p} | `));
+  console.log(rowOutput);
+  rowOutput = ''; // cleaning the variable so we can re-use it
+
+  // staring from the second element of the array - index = 1
+  for (let i = 1; i < primesArray.length; i++) {
+    // output the original element of the primes array vertically
+    rowOutput += `${primesArray[i]} | `;
+    // multiplying each subsequent element with all the primes of the original primes array
+    for (let j = 1; j < primesArray.length; j++) {
+      rowOutput += `${primesArray[i] * primesArray[j]} | `;
+    }
+    // output the new row
+    console.log(rowOutput);
+    rowOutput = ''; // cleaning the variable so we can re-use it
+  }
+}
