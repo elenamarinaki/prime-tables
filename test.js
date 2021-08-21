f735144144b7968663e8ab41e90a5cc9fa45d510
Elena <rhapsodyenmauve@gmail.com>
2021-08-21 21:29:14 +0100

    more tests for the giveNPrimeNumbers function

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+// first I am testing the giveNPrimeNumbers function to make sure the one dimensional array it returns is what is expected
+//  ---------------------------------------------------------------------
 test('Testing the primes generator function:', () => {
   // equality does not work here, arrays are reference objects
   equal(giveNPrimeNumbers(3), [1, 2, 3, 5]);
